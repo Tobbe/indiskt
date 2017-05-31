13a9de4f9d90e3c3d965446e79e9f596accf9bf9
Tobbe Lundberg <tobbe@tlundberg.com>
2017-05-31 22:00:47 +0200

    Put dishes component css and html in separate files

--- src/app/dishes.component.ts
+++ src/app/dishes.component.ts
@@ -5,69 +5,9 @@ import { DishService } from './dish.service';
 
 @Component({
   selector: 'my-dishes',
-  template: `
-    <h2>Menu</h2>
-    <ul class="dishes">
-      <li *ngFor="let dish of dishes"
-        [class.selected]="dish === selectedDish"
-        (click)="onSelect(dish)">
-
-        <span class="badge">{{dish.id}}</span> {{dish.name}}
-      </li>
-    </ul>
-    <dish-detail [dish]="selectedDish"></dish-detail>
-    `,
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .dishes {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .dishes li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .dishes li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .dishes li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .dishes .text {
-      position: relative;
-      top: -3px;
-    }
-    .dishes .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `],
+  templateUrl: './dishes.component.html',
+  styleUrls: [ './dishes.component.css' ],
 })
-
 export class DishesComponent implements OnInit {
   dishes: Dish[];
   selectedDish: Dish;
