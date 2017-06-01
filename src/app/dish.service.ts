38f07396b5442b5cc693d61bb067b9494f3ac59f
Tobbe Lundberg <tobbe@tlundberg.com>
2017-06-01 03:37:47 +0200

    Fetch dishes from in-memory http server

--- src/app/dish.service.ts
+++ src/app/dish.service.ts
@@ -1,17 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Dish } from './dish';
-import { DISHES } from './mock-dishes';
 
 @Injectable()
 export class DishService {
+  private dishesUrl = 'api/dishes';
+
+  constructor(private http: Http) { }
+
   getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+    return this.http
+      .get(this.dishesUrl)
+      .toPromise()
+      .then(response => response.json().data as Dish[])
+      .catch(this.handleError);
   }
 
   getDish(id: number): Promise<Dish> {
-    return this
-      .getDishes()
-      .then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.dishesUrl}/${id}`;
+
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(response => response.json().data as Dish)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+
+    return Promise.reject(error.message || error);
   }
 }
