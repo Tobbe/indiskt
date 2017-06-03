510210ff3741f6e3b431264b9cf5c45a8f2bb911
Tobbe Lundberg <tobbe@tlundberg.com>
2017-06-03 10:01:23 +0200

    Submit indian food orders to firebase

--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 import { Dish } from '../dish';
 import { DishService } from '../dish.service';
 import { Order } from '../order.model';
@@ -14,14 +16,17 @@ export class OrderFormComponent implements OnInit {
   dishes: Dish[];
   selectedDish: Dish;
   model = new Order('tlb', 'Chicken Vindaloo', 'xhot', 'little', 'zero', '');
+  orders: FirebaseListObservable<any[]>;
 
-  constructor(private dishService: DishService, private router: Router) { }
+  constructor(private dishService: DishService, private router: Router, private db: AngularFireDatabase) { }
 
   ngOnInit(): void {
+    this.orders = this.db.list('/orders');
   }
 
   onSubmit(): void {
     console.log('submit the form', this.model);
+    this.orders.push(this.model);
   }
 
   get currentOrder(): string {
